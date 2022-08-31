7e396aa22c384f5472809e4b54676f9d2f313d18
jiangshan04 <jiangshan04@picclife.cn>
2022-08-31 18:01:03 +0800

    f: add homepage structure

--- jason-blog/src/App.vue
+++ jason-blog/src/App.vue
@@ -1,20 +1,62 @@
 <template>
-  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-    <el-tab-pane label="User" name="first">User</el-tab-pane>
-    <el-tab-pane label="Config" name="second">Config</el-tab-pane>
-    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
-    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+  <el-radio-group v-model="selectedBook" style="margin-bottom: 30px">
+    <el-radio-button v-for="book in books" :key="book.index" :label="book">{{
+      book.name
+    }}</el-radio-button>
+  </el-radio-group>
+  <el-tabs tab-position="left" class="demo-tabs">
+    <el-tab-pane
+      v-for="chapter in selectedBook.chapters"
+      :key="chapter.index"
+      :label="chapter.name"
+    >
+      {{ chapter.name }}
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
-import type { TabsPaneContext } from "element-plus";
 
-const activeName = ref("first");
-
-const handleClick = (tab: TabsPaneContext, event: Event) => {
-  console.log(tab, event);
-};
+let tabIndex = 2;
+const books = ref([
+  {
+    index: "0",
+    name: "Linear Algebra",
+    chapters: [
+      {
+        index: "0",
+        name: "线性代数中的线性方程组",
+      },
+      {
+        index: "1",
+        name: "矩阵代数",
+      },
+      {
+        index: "1",
+        name: "矩阵代数",
+      },
+    ],
+  },
+  {
+    index: "1",
+    name: "Java In Concurrence",
+    chapters: [
+      {
+        index: "0",
+        name: "Fundamentals",
+      },
+      {
+        index: "1",
+        name: "Fundamentals",
+      },
+      {
+        index: "1",
+        name: "Fundamentals",
+      },
+    ],
+  },
+]);
+const selectedBook = ref(books.value[0]);
 </script>
 <style>
 .demo-tabs > .el-tabs__content {
